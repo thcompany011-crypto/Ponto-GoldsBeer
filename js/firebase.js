9afb4adcda179af185b00ecb7d4c952023912db2
thcompany011-crypto <thcompany011@gmail.com>
2026-07-02 15:53:36 -0300

    Initialize Firebase app with configuration

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "sistemadeponto-2a85d.firebaseapp.com",
+  projectId: "sistemadeponto-2a85d",
+  storageBucket: "sistemadeponto-2a85d.appspot.com",
+  messagingSenderId: "849213562202",
+  appId: "1:849213562202:web:b2c54de90144e8997a763"
+};
+
+export const app = initializeApp(firebaseConfig);
+
