97d84422780135de6a145e09534e15c51243aa0b
thcompany011-crypto <thcompany011@gmail.com>
2026-09-09 23:56:08 -0300

    Upgrade service worker cache version and manage old caches
    Updated cache name to 'ponto-pro-v2' and added activation event to manage old caches.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = 'ponto-pro-v1';
+const cacheName = 'ponto-pro-v2';
 const assets = [
   '/',
   '/index.html',
@@ -7,15 +7,25 @@ const assets = [
   '/css/style.css',
   '/js/firebase.js',
   '/js/auth.js',
-  '/js/dashboard.js',
-  '/js/ponto.js'
+  '/js/login.js',
+  '/js/dashboard.js'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('activate', (e) => {
+  // Remove caches de versões antigas para evitar servir JS obsoleto para os usuários
+  e.waitUntil(
+    caches.keys().then(chaves =>
+      Promise.all(chaves.filter(c => c !== cacheName).map(c => caches.delete(c)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
 });
-
